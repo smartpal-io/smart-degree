1d713fa0cfa37e23d6f2af91d1a8accd70ed9709
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2018-06-04 13:08:10 +0200

    smartkit integration

--- test/TestSmartDegree.js
+++ test/TestSmartDegree.js
@@ -5,9 +5,9 @@ contract('SmartDegree', function() {
     var result;
     return SmartDegree.deployed().then(function(instance) {
       smartDegree = instance;
-      smartDegree.addDegreeHash('0x000000000000000000000000000000000000000000000000000000000000001',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
+      smartDegree.deliverDegree('0x000000000000000000000000000000000000000000000000000000000000001',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
     }).then(function() {
-      return smartDegree.getHash('0x000000000000000000000000000000000000000000000000000000000000001');
+      return smartDegree.getDegreeHash('0x000000000000000000000000000000000000000000000000000000000000001');
     }).then(function(hash) {
       result = hash;
     }).then(function() {
@@ -19,33 +19,33 @@ contract('SmartDegree', function() {
     var result;
     return SmartDegree.deployed().then(function(instance) {
       smartDegree = instance;
-      smartDegree.addDegreeHash('0x000000000000000000000000000000000000000000000000000000000000002', "0xa7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
+      smartDegree.deliverDegree('0x000000000000000000000000000000000000000000000000000000000000002', "0xa7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
     }).then(function() {
-      smartDegree.addDegreeHash('0x000000000000000000000000000000000000000000000000000000000000003', "0xf7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9f");
+      smartDegree.deliverDegree('0x000000000000000000000000000000000000000000000000000000000000003', "0xf7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9f");
     }).then(function() {
-      return smartDegree.getHash('0x000000000000000000000000000000000000000000000000000000000000002');
+      return smartDegree.getDegreeHash('0x000000000000000000000000000000000000000000000000000000000000002');
     }).then(function(hash) {
       result = hash;
     }).then(function() {
       assert.ok(result,"0xa7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c", "Error valid hash not found");
     });
   });
-   it("should verify the hash", function() {
+   it("should isValid the hash", function() {
     var smartDegree;
     var result;
     return SmartDegree.deployed().then(function(instance) {
       smartDegree = instance;
-      return smartDegree.addDegreeHash('0x000000000000000000000000000000000000000000000000000000000000004', "0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
+      return smartDegree.deliverDegree('0x000000000000000000000000000000000000000000000000000000000000004', "0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
     }).then(function(result) {
-      return smartDegree.verify('0x000000000000000000000000000000000000000000000000000000000000004',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
+      return smartDegree.isValid('0x000000000000000000000000000000000000000000000000000000000000004',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
     }).then(function(isValid) {
       result = isValid;
     }).then(function() {
       assert.ok(result, "Error valid hash not detected");
     }).then(function() {
-      return smartDegree.addDegreeHash('0x000000000000000000000000000000000000000000000000000000000000005', "0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
+      return smartDegree.deliverDegree('0x000000000000000000000000000000000000000000000000000000000000005', "0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9c");
     }).then(function(result) {
-      return smartDegree.verify('0x000000000000000000000000000000000000000000000000000000000000005',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9f");
+      return smartDegree.isValid('0x000000000000000000000000000000000000000000000000000000000000005',"0xe7834034bd059ecf00b0661f88f1e7242450bf1951c1e76803e80ce4182e2e9f");
     }).then(function(isValid) {
       result = isValid;
     }).then(function() {
